1a7ccf16eabda72ddba8392399f1b5a715738622
St-thomas-Aquinas <maxkuria254@gmail.com>
2025-08-20 14:15:31 +0300

    port change

--- app.py
+++ app.py
@@ -57,6 +57,6 @@ def home():
     return "✅ Twilio + HuggingFace WhatsApp bot is running on Render!"
 
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render requires $PORT
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
